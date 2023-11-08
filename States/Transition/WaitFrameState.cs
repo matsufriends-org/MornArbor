f008ddfc84d29fa83309af259ee08ba571e9380f
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 05:39:55 +0900

    [feat] WaitFrameStateの作成

---
+++ States/Transition/WaitFrameState.cs
@@ -0,0 +1,26 @@
+﻿using Arbor;
+using UnityEngine;
+
+namespace MornArbor.States
+{
+    public class WaitFrameState : StateBehaviour
+    {
+        [SerializeField] private FlexibleField<int> _waitFrame;
+        [SerializeField] private StateLink _next;
+        private int _elapsedFrame;
+
+        public override void OnStateBegin()
+        {
+            _elapsedFrame = 0;
+        }
+
+        public override void OnStateUpdate()
+        {
+            _elapsedFrame++;
+            if (_elapsedFrame >= _waitFrame.value)
+            {
+                Transition(_next);
+            }
+        }
+    }
+}
