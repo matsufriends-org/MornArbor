eac5dbf0eed1e3fa9bb253321b5db8b5175e8336
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 02:26:19 +0900

    [feat] CinemachineEnablerの作成

---
+++ States/Cinemachine/CinemachineEnablerState.cs
@@ -0,0 +1,17 @@
+﻿using Arbor;
+using Cinemachine;
+using UnityEngine;
+
+namespace MornArbor.States
+{
+    public sealed class CinemachineEnablerState : StateBehaviour
+    {
+        [SerializeField] private FlexibleField<CinemachineVirtualCamera> _camera;
+        [SerializeField] private FlexibleField<bool> _isActive;
+
+        public override void OnStateBegin()
+        {
+            _camera.value.enabled = _isActive.value;
+        }
+    }
+}
