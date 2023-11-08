8c44fce2f98e0e99c4fa2699ca7e689ea25dcae6
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 02:35:23 +0900

    [refactor]

--- States/Transition/TransitionIndexState.cs
+++ States/Transition/TransitionIndexState.cs
@@ -6,12 +6,12 @@ namespace MornArbor.States
     public sealed class TransitionIndexState : StateBehaviour
     {
         [SerializeField] private FlexibleField<int> _index;
-        [SerializeField] private StateLink[] _nextLinks;
+        [SerializeField] private StateLink[] _next;
 
         public override void OnStateBegin()
         {
-            var index = Mathf.Clamp(_index.value, 0, _nextLinks.Length);
-            Transition(_nextLinks[index]);
+            var index = Mathf.Clamp(_index.value, 0, _next.Length);
+            Transition(_next[index]);
         }
     }
 }
