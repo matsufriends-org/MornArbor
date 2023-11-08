34419f75fc8bdfd46d2f202c17e2f179da37cad4
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 02:35:46 +0900

    [feat] アプリ終了Stateの作成

---
+++ States/Util/ApplicationCloseState.cs
@@ -0,0 +1,17 @@
+﻿using Arbor;
+using UnityEditor;
+
+namespace MornArbor.States
+{
+    public class ApplicationCloseState : StateBehaviour
+    {
+        public override void OnStateBegin()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
