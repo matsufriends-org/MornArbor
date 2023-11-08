3796c2a26f6cf4727b687c6a6c85abac0f867217
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 03:05:13 +0900

    [rename]

---
+++ States/MornUI/MornUIControllerResetFocusState.cs
@@ -0,0 +1,16 @@
+﻿using Arbor;
+using MornUI;
+using UnityEngine;
+
+namespace MornArbor.States
+{
+    public class MornUIControllerResetFocusState : StateBehaviour
+    {
+        [SerializeField] private MornUIControllerMono _controller;
+
+        public override void OnStateBegin()
+        {
+            _controller.ResetFocus();
+        }
+    }
+}
