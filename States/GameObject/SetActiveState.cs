ef80ec367b932b8e6fe74769adbe70fefe6f6496
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 02:27:08 +0900

    [feat] SetActiveStateの作成

--- States/GameObject/SetActiveState.cs
+++ States/GameObject/SetActiveState.cs
@@ -5,12 +5,12 @@ namespace MornArbor.States
 {
     public class SetActiveState : StateBehaviour
     {
-        [SerializeField] private GameObject _target;
+        [SerializeField] private FlexibleField<GameObject> _target;
         [SerializeField] private FlexibleField<bool> _isActive;
 
         public override void OnStateBegin()
         {
-            _target.SetActive(_isActive.value);
+            _target.value.SetActive(_isActive.value);
         }
     }
 }
