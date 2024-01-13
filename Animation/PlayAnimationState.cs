2f737814a292d0b8da196cc030f2647d055ac2dd
matsu_friends <pikapikan0526@gmail.com>
2024-01-13 09:55:28 +0900

    PlayAnimationにて再生後遷移を可能に

--- Animation/PlayAnimationState.cs
+++ Animation/PlayAnimationState.cs
@@ -8,10 +8,21 @@ namespace MornArbor
         [SerializeField] private Animator _animator;
         [SerializeField] private AnimationClip _animationClip;
         [SerializeField] private float _duration;
+        [SerializeField] private StateLink _nextState;
+        private float _startTime;
 
         public override void OnStateBegin()
         {
             _animator.CrossFadeInFixedTime(_animationClip.name, _duration);
+            _startTime = Time.time;
+        }
+
+        public override void OnStateUpdate()
+        {
+            if (Time.time - _startTime > _animationClip.length)
+            {
+                Transition(_nextState);
+            }
         }
     }
 }
