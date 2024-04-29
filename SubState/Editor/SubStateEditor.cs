f2f721eadd8cb81110fe688acd3ec37a3e233ab2
matsu_friends <pikapikan0526@gmail.com>
2024-04-30 03:23:27 +0900

    SubStateを仮作成

---
+++ SubState/Editor/SubStateEditor.cs
@@ -0,0 +1,53 @@
+﻿using Arbor;
+using UnityEditor;
+using UnityEngine;
+
+namespace MornArbor
+{
+    [CustomEditor(typeof(SubState))]
+    public sealed class SubStateEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var subModuleProperty = serializedObject.FindProperty("subModule");
+            if (subModuleProperty == null)
+            {
+                return;
+            }
+
+            var subModule = subModuleProperty.objectReferenceValue as GameObject;
+            if (subModule == null)
+            {
+                return;
+            }
+
+            var arbor = subModule.GetComponent<ArborFSMInternal>();
+            if (arbor == null)
+            {
+                return;
+            }
+
+            GUILayout.BeginVertical(GUI.skin.box);
+            GUILayout.Label("StateExitNames");
+            
+            // Indent
+            EditorGUI.indentLevel++;
+            foreach (var stateExit in arbor.GetComponents<StateExit>())
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(EditorGUI.indentLevel * 10);
+                GUILayout.Label(stateExit.ExitFlagName);
+                if (GUILayout.Button("Copy", GUILayout.Width(40)))
+                {
+                    EditorGUIUtility.systemCopyBuffer = stateExit.ExitFlagName;
+                }
+
+                GUILayout.EndHorizontal();
+            }
+            
+            EditorGUI.indentLevel--;
+            GUILayout.EndVertical();
+        }
+    }
+}
---
+++ SubState/StateExit.cs
@@ -0,0 +1,17 @@
+﻿using Arbor;
+using UnityEngine;
+
+namespace MornArbor
+{
+    public class StateExit : StateBehaviour
+    {
+        [SerializeField] private string exitFlagName;
+        public string ExitFlagName => exitFlagName;
+        
+        public override void OnStateBegin()
+        {
+            var stateExitTrigger = GetComponent<StateExitTrigger>();
+            stateExitTrigger.Exit(exitFlagName);
+        }
+    }
+}
