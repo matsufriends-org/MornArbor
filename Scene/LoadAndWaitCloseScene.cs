e6bcb2ffd4966c33f27e8fc1e6390109c10dfb67
matsu_friends <pikapikan0526@gmail.com>
2024-05-12 10:04:55 +0900

    Scene読み込みの機能を拡充

---
+++ Scene/LoadAndWaitCloseScene.cs
@@ -0,0 +1,32 @@
+﻿using Arbor;
+using MornScene;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MornArbor
+{
+    public class LoadAndWaitCloseSceneState : StateBehaviour
+    {
+        [SerializeField] private FlexibleField<MornSceneObject> _scene;
+        [SerializeField] private StateLink _next;
+        private AsyncOperation _loadTask;
+        private Scene _loadScene;
+
+        public override void OnStateBegin()
+        {
+            _loadTask = SceneManager.LoadSceneAsync(_scene.value, LoadSceneMode.Additive);
+            _loadScene = SceneManager.GetSceneByName(_scene.value);
+        }
+
+        public override void OnStateUpdate()
+        {
+            if (_loadTask == null || _loadTask.isDone)
+            {
+                if (!_loadScene.isLoaded)
+                {
+                    Transition(_next);
+                }
+            }
+        }
+    }
+}
---
+++ Scene/SwapScene.cs
@@ -0,0 +1,35 @@
+﻿using Arbor;
+using MornScene;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MornArbor
+{
+    public class SwapSceneState : StateBehaviour
+    {
+        [SerializeField] private FlexibleField<MornSceneObject> _scene;
+        private AsyncOperation _task;
+        private bool _isUnloadCurrent;
+
+        public override void OnStateBegin()
+        {
+            _task = SceneManager.LoadSceneAsync(_scene.value, LoadSceneMode.Additive);
+            _isUnloadCurrent = false;
+        }
+
+        public override void OnStateUpdate()
+        {
+            if (_task == null || _task.isDone)
+            {
+                if (_isUnloadCurrent)
+                {
+                    return;
+                }
+
+                SceneManager.UnloadSceneAsync(gameObject.scene);
+                _task = null;
+                _isUnloadCurrent = true;
+            }
+        }
+    }
+}
