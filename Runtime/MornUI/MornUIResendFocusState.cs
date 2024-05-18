d6cf07659695bda4c512351759bf50933299a7eb
matsu_friends <pikapikan0526@gmail.com>
2024-05-19 08:31:19 +0900

    [feat] Focusのみ再設定するStateを作成

---
+++ Runtime/MornUI/MornUIResendFocusState.cs
@@ -0,0 +1,16 @@
+﻿using Arbor;
+using MornUI;
+using UnityEngine;
+
+namespace MornArbor
+{
+    public class MornUIResendFocusState : StateBehaviour
+    {
+        [SerializeField] private MornUIControllerMono _controller;
+
+        public override void OnStateBegin()
+        {
+            _controller.ResendFocus();
+        }
+    }
+}
