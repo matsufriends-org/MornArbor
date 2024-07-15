f20529bcc4efe14768a130fda770064ab775552f
matsufriends <pikapikan0526@gmail.com>
2024-07-15 20:12:53 +0900

    MornUIを撤廃

---
+++ MornBeat/MornBeatPlayState.cs
@@ -0,0 +1,17 @@
+﻿using Arbor;
+using UnityEngine;
+using VContainer;
+
+namespace MornBeat
+{
+    public class MornBeatPlayState : StateBehaviour
+    {
+        [SerializeField] private MornBeatMemoSo _beatMemo;
+        [Inject] private MornBeatControllerMono _beatController;
+
+        public override void OnStateBegin()
+        {
+            _beatController.InitializeBeat(_beatMemo);
+        }
+    }
+}
---
+++ MornBeat/MornBeatResetState.cs
@@ -0,0 +1,15 @@
+﻿using Arbor;
+using VContainer;
+
+namespace MornBeat
+{
+    public class MornBeatResetState : StateBehaviour
+    {
+        [Inject] private MornBeatControllerMono _beatController;
+
+        public override void OnStateBegin()
+        {
+            _beatController.ResetBeat();
+        }
+    }
+}
