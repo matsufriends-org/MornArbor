c45f0fd00d9952b8487fd9cbacd58361b00848c2
matsufriends <pikapikan0526@gmail.com>
2024-07-16 01:51:48 +0900

    Aniamtionステート追加

---
+++ Transition/WaitRandomTimeCurveState.cs
@@ -0,0 +1,22 @@
+﻿using Arbor;
+using UnityEngine;
+
+namespace MornArbor
+{
+    public class WaitRandomTimeCurveState : StateBehaviour
+    {
+        [SerializeField] private AnimationCurve _curve;
+        [SerializeField] private StateLink _next;
+        private float _transitionTime;
+
+        public override void OnStateBegin()
+        {
+            _transitionTime = Time.time +_curve.Evaluate(Random.value);
+        }
+
+        public override void OnStateUpdate()
+        {
+            if (Time.time >= _transitionTime) Transition(_next);
+        }
+    }
+}
