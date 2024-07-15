d5986fd899170b496f638f9943f4d5b0886c1abc
matsufriends <pikapikan0526@gmail.com>
2024-07-15 14:06:03 +0900

    nextなし

--- MornScene/ChangeSceneState.cs
+++ MornScene/ChangeSceneState.cs
@@ -8,17 +8,10 @@ namespace MornScene
     public class ChangeSceneState : StateBehaviour
     {
         [SerializeField] private FlexibleField<MornSceneObject> _scene;
-        [SerializeField] private StateLink _next;
-        private UniTask _task;
 
         public override void OnStateBegin()
         {
-            _task = SceneManager.LoadSceneAsync(_scene.value, LoadSceneMode.Additive).ToUniTask();
-        }
-
-        public override void OnStateUpdate()
-        {
-            if (_task.Status != UniTaskStatus.Pending) Transition(_next);
+            SceneManager.LoadSceneAsync(_scene.value).ToUniTask();
         }
     }
 }
