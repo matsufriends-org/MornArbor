4e9cb9bb0393977755eca35e291a95a3df4537f8
matsufriends <pikapikan0526@gmail.com>
2024-07-19 03:55:38 +0900

    SetActive

---
+++ Common/SetActiveProcess.cs
@@ -0,0 +1,17 @@
+﻿using Arbor;
+using UnityEngine;
+
+namespace MornArbor
+{
+    public class SetActiveProcess : StateBehaviour, IProcessState
+    {
+        [SerializeField] private GameObject _target;
+        [SerializeField] private bool _isActive;
+        public bool IsActive => _target.activeSelf != _isActive;
+
+        public override void OnStateBegin()
+        {
+            _target.SetActive(_isActive);
+        }
+    }
+}
