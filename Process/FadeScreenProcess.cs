a43572082c329d713e0a3b05886167531aeaf621
matsufriends <pikapikan0526@gmail.com>
2024-08-08 21:17:02 +0900

    refactor

--- Process/FadeScreenProcess.cs
+++ Process/FadeScreenProcess.cs
@@ -9,9 +9,10 @@ namespace MornArbor.Process
         [SerializeField] [Range(0, 1f)] private float _endValue;
         [SerializeField] private float _duration = 1;
         [Inject] private IMornScreenFade _screenFade;
-        private float _startTime;
+        private float _startTime = -1;
         private float _startValue;
-        public override float Progress => _duration > 0 ? Mathf.Clamp01((Time.time - _startTime) / _duration) : 1;
+        public override float Progress => _startTime < 0 ? 0 :
+            _duration > 0 ? Mathf.Clamp01((Time.time - _startTime) / _duration) : 1;
 
         public override void OnStateBegin()
         {
@@ -23,11 +24,6 @@ namespace MornArbor.Process
         {
             var t = Mathf.Clamp01((Time.time - _startTime) / _duration);
             _screenFade.Value = Mathf.Lerp(_startValue, _endValue, t);
-        }
-
-        public override void OnStateEnd()
-        {
-            _screenFade.Value = _endValue;
         }
     }
 }
--- Process/FadeVolumeProcess.cs
+++ Process/FadeVolumeProcess.cs
@@ -4,12 +4,13 @@ namespace MornArbor.Process
 {
     public class FadeVolumeProcess : ProcessBase
     {
+        [SerializeField] private AudioSource _source;
         [SerializeField] [Range(0, 1f)] private float _endValue;
         [SerializeField] private float _duration = 1;
-        [SerializeField] private AudioSource _source;
-        private float _startTime;
+        private float _startTime = -1;
         private float _startValue;
-        public override float Progress => _duration > 0 ? Mathf.Clamp01((Time.time - _startTime) / _duration) : 1;
+        public override float Progress => _startTime < 0 ? 0 :
+            _duration > 0 ? Mathf.Clamp01((Time.time - _startTime) / _duration) : 1;
 
         public override void OnStateBegin()
         {
@@ -21,11 +22,6 @@ namespace MornArbor.Process
         {
             var t = Mathf.Clamp01((Time.time - _startTime) / _duration);
             _source.volume = Mathf.Lerp(_startValue, _endValue, t);
-        }
-
-        public override void OnStateEnd()
-        {
-            _source.volume = _endValue;
         }
     }
 }
