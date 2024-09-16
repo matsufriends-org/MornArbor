956f50f1b3c9ac6a15a7f7d556616d6af2d98c19
matsufriends <pikapikan0526@gmail.com>
2024-09-17 00:29:59 +0900

    SubStateが自動発火しないように

--- Sequence/SubState.cs
+++ Sequence/SubState.cs
@@ -9,15 +9,26 @@ namespace MornArbor.Sequence
         [SerializeField] private ArborFSMInternal _prefab;
         [SerializeField] private bool _instantiate;
 
+        protected override void OnValidate()
+        {
+            if (_prefab != null)
+            {
+                _prefab.enabled = false;
+            }
+        }
+
         protected override IEnumerator Load()
         {
             var subState = _instantiate ? Instantiate(_prefab, transform) : _prefab;
+            _prefab.enabled = true;
+            subState.Transition(subState.startStateID, TransitionTiming.Immediate);
             var provider = subState.gameObject.AddComponent<SubStateExitCodeProvider>();
             while (string.IsNullOrEmpty(provider.ExitCode))
             {
                 yield return null;
             }
 
+            _prefab.enabled = false;
             if (_instantiate)
             {
                 Destroy(subState);
