7e1e942639a8998abb57fd824031249161cecaba
matsufriends <pikapikan0526@gmail.com>
2025-05-29 18:43:20 +0900

    PlaySimpleSoundActionの修正

--- Action/PlaySimpleSoundAction.cs
+++ Action/PlaySimpleSoundAction.cs
@@ -2,18 +2,17 @@
 using Arbor;
 using MornSound;
 using UnityEngine;
-using VContainer;
 
 namespace MornArbor.Action
 {
     public sealed class PlaySimpleSoundAction : StateBehaviour
     {
+        [SerializeField] private AudioSource _audioSource;
         [SerializeField] private AudioClip _audioClip;
-        [Inject] private IMornSoundSimple _soundSimple;
 
         public override void OnStateBegin()
         {
-            _soundSimple.Play(_audioClip);
+            _audioSource.MornPlay(_audioClip);
         }
     }
 }
