d257a6c6f313edb5fc8a0c255e1443eed97686ca
matsufriends <pikapikan0526@gmail.com>
2025-06-15 20:29:45 +0900

    refactor

---
+++ Obsolete/ObsoletePlayAnimationProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace MornArbor
+{
+    [Obsolete]
+    public class ObsoletePlayAnimationProcess : ProcessBase
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private AnimationClip _animationClip;
+        [SerializeField] private float _transitionDuration;
+        private float _startTime;
+        public override float Progress => _animationClip ? Mathf.Clamp01((Time.time - _startTime) * _animator.speed / _animationClip.length) : 1f;
+
+        public override void OnStateBegin()
+        {
+            _animator.CrossFadeInFixedTime(_animationClip.name, _transitionDuration);
+            _startTime = Time.time;
+        }
+    }
+}
--- Process/PlayAnimationProcess.cs
+++ Process/PlayAnimationProcess.cs
@@ -1,19 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using Arbor;
+using MornUtil;
+using UnityEngine;
 
 namespace MornArbor
 {
+    [Obsolete]
     public class PlayAnimationProcess : ProcessBase
     {
         [SerializeField] private Animator _animator;
-        [SerializeField] private AnimationClip _animationClip;
-        [SerializeField] private float _transitionDuration;
+        [SerializeField] private BindAnimatorClip _bind;
+        [SerializeField] private float _duration;
+        [SerializeField] private StateLink _onComplete;
         private float _startTime;
-        public override float Progress => _animationClip ? Mathf.Clamp01((Time.time - _startTime) * _animator.speed / _animationClip.length) : 1f;
+        public override float Progress =>
+            _bind.Clip ? Mathf.Clamp01((Time.time - _startTime) * _animator.speed / _bind.Clip.length) : 1f;
 
         public override void OnStateBegin()
         {
-            _animator.CrossFadeInFixedTime(_animationClip.name, _transitionDuration);
+            _animator.CrossFadeInFixedTime(_bind.Clip.name, _duration);
             _startTime = Time.time;
+        }
+
+        public override void OnStateUpdate()
+        {
+            if (Progress >= 1f)
+            {
+                Transition(_onComplete);
+            }
         }
     }
 }
