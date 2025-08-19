e3d23c5a4e6b21b28db3e196ccf5439d76611a8e
matsufriends <pikapikan0526@gmail.com>
2025-08-20 03:57:51 +0900

    MornTweenの一括制御ステート追加

---
+++ Tween/MornTweenProcess.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Arbor;
+using MornLib._Morn.MornTween;
+using UnityEngine;
+
+namespace MornArbor.Tween
+{
+    public class MornTweenProcess : ProcessBase
+    {
+        [SerializeField] private List<MornTweenBase> _tweenList;
+        [SerializeField] private StateLink _nextState;
+        private float _progress;
+        public override float Progress => _progress;
+
+        public override void OnStateBegin()
+        {
+            foreach (var tween in _tweenList)
+            {
+                tween.TweenStart();
+            }
+
+            _progress = 0f;
+        }
+
+        public override void OnStateUpdate()
+        {
+            var progressSum = 0f;
+            foreach (var tween in _tweenList)
+            {
+                progressSum += tween.Progress;
+            }
+
+            _progress = Mathf.Clamp01(progressSum / _tweenList.Count);
+            if (_progress >= 1f)
+            {
+                Transition(_nextState);
+            }
+        }
+    }
+}
