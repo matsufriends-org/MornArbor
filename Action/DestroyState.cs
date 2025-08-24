c79a7a1a08c74d67e3618cd629510496b003e1a6
matsufriends <pikapikan0526@gmail.com>
2025-08-25 03:48:54 +0900

    DestroyStateの追加

---
+++ Action/DestroyState.cs
@@ -0,0 +1,20 @@
+﻿using Arbor;
+using UnityEngine;
+
+namespace MornArbor
+{
+    public class DestroyState : StateBehaviour
+    {
+        [SerializeField] private GameObject _target;
+
+        public override void OnStateBegin()
+        {
+            if (_target == null)
+            {
+                return;
+            }
+
+            Destroy(_target);
+        }
+    }
+}
