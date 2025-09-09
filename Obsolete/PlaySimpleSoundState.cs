0023ae9a4fee1de0f155e0b90f8b5fa0c949d269
matsufriends <pikapikan0526@gmail.com>
2025-09-09 16:41:45 +0900

    Obsolete化

--- Obsolete/PlaySimpleSoundState.cs
+++ Obsolete/PlaySimpleSoundState.cs
@@ -1,10 +1,12 @@
 ﻿#if USE_MORN_SOUND
+using System;
 using Arbor;
 using MornSound;
 using UnityEngine;
 
 namespace MornArbor
 {
+    [Obsolete("MornSoundPlayOneShotStateやMornSoundPlayOneShotStateを使用してください。")]
     public sealed class PlaySimpleSoundState : StateBehaviour
     {
         [SerializeField] private AudioSource _audioSource;
